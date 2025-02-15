e07f0d4dfa302f4c53eb43295c4557cff06a96e4
joshiemoore <joshiemoore98@gmail.com>
2025-02-15 12:23:11 -0500

    Add tests

--- snakehdl/bops.py
+++ snakehdl/bops.py
@@ -31,25 +31,25 @@ class BOp:
 
   # only for BOps.INPUT
   input_id: Optional[str] = None
-  input_bits: Optional[Iterable[int]] = None
+  bits: Optional[Iterable[int]] = None
 
   # only for BOps.OUTPUT
   outputs: Optional[dict] = None
 
   # only for BOps.CONST
-  const: Optional[str] = None
+  val: Optional[int] = None
 
 # special operations
-def input(id: str, bits: Optional[Iterable[int]]=None) -> BOp: return BOp(op=BOps.INPUT, input_id=id, input_bits=bits if bits else [0])
-def output(**kwargs: BOp) -> BOp: return BOp(op=BOps.OUTPUT, outputs=kwargs)
-def const(val: str) -> BOp: return BOp(op=BOps.CONST, const=val)
-def noop() -> BOp: return BOp(op=BOps.NOOP)
+def input_bits(id: str, bits: Optional[Iterable[int]]=None) -> BOp: return BOp(BOps.INPUT, input_id=id, bits=bits if bits else [0])
+def output(**kwargs: BOp) -> BOp: return BOp(BOps.OUTPUT, outputs=kwargs)
+def const(val: str|int) -> BOp: return BOp(BOps.CONST, val=int(val, 2) if isinstance(val, str) else val)
+def noop() -> BOp: return BOp(BOps.NOOP)
 
 # combinational operations
-def neg(a: BOp) -> BOp: return BOp(op=BOps.NOT, src=(a,))
-def conj(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.AND, src=(a, b))
-def nand(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.NAND, src=(a, b))
-def disj(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.OR, src=(a, b))
-def nor(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.NOR, src=(a, b))
-def xor(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.XOR, src=(a, b))
-def xnor(a: BOp, b: BOp) -> BOp: return BOp(op=BOps.XNOR, src=(a, b))
+def neg(a: BOp) -> BOp: return BOp(BOps.NOT, src=(a,))
+def conj(a: BOp, b: BOp) -> BOp: return BOp(BOps.AND, src=(a, b))
+def nand(a: BOp, b: BOp) -> BOp: return BOp(BOps.NAND, src=(a, b))
+def disj(a: BOp, b: BOp) -> BOp: return BOp(BOps.OR, src=(a, b))
+def nor(a: BOp, b: BOp) -> BOp: return BOp(BOps.NOR, src=(a, b))
+def xor(a: BOp, b: BOp) -> BOp: return BOp(BOps.XOR, src=(a, b))
+def xnor(a: BOp, b: BOp) -> BOp: return BOp(BOps.XNOR, src=(a, b))
---
+++ tests/test_bops.py
@@ -0,0 +1,65 @@
+import pytest
+from snakehdl.bops import *
+
+
+class TestCreateBOps:
+  #### special operations ####
+  def test_input_bits(self):
+    op = input_bits('a')
+    assert op.op is BOps.INPUT
+    assert op.input_id == 'a'
+    assert op.bits == [0]
+
+    op = input_bits('b', range(2,6))
+    assert len(op.bits) == 4
+    for i in range(2, 6): assert i in op.bits
+
+  def test_output(self):
+    op = output()
+    assert op.op is BOps.OUTPUT
+    assert op.outputs == {}
+
+    op = output(out_a=const('0'), out_b=const('1'))
+    assert op.outputs == {'out_a': const('0'), 'out_b': const('1')}
+
+  def test_const(self):
+    op = const('1010')
+    assert op.op is BOps.CONST
+    assert op.val == 0b1010
+
+    op = const(123)
+    assert op.val == 123
+
+    with pytest.raises(ValueError):
+      const('asdf')
+
+    assert const(13) == const(13)
+    assert const(8) != const(14)
+
+  def test_noop(self):
+    op = noop()
+    assert op.op is BOps.NOOP
+
+  #### combinational operations ####
+  def test_neg(self):
+    op = neg(const(1))
+    assert op.op is BOps.NOT
+    assert len(op.src) == 1
+    assert op.src[0] == const('1')
+
+  def test_binary_combinational_ops(self):
+    ops = {
+      BOps.AND: conj,
+      BOps.NAND: nand,
+      BOps.OR: disj,
+      BOps.NOR: nor,
+      BOps.XOR: xor,
+      BOps.XNOR: xnor,
+    }
+
+    for bop, func in ops.items():
+      op = func(const(0), const(1))
+      assert op.op is bop
+      assert len(op.src) == 2
+      assert op.src[0] == const(0)
+      assert op.src[1] == const(1)
