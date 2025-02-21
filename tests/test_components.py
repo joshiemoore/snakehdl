76b97af4a3bfde0b0d8f46381fb6f671a6927e1b
joshiemoore <joshiemoore98@gmail.com>
2025-02-21 09:43:41 -0500

    Add mux test

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,11 +1,12 @@
 import dill
 from typing import Callable
+import pytest
 from snakehdl import (
   BOp,
   input_bits, output,
 )
 from snakehdl.compiler import PythonCompiler
-from snakehdl.components import adder
+from snakehdl.components import adder, mux
 
 
 def _get_func(tree: BOp) -> Callable:
@@ -20,3 +21,24 @@ class TestArithmeticComponents:
     for i in range(7): assert func(a=i, b=i, cin=0) == {'res': i + i, 'cout': 0}
     assert func(a=0, b=0, cin=1) == {'res': 1, 'cout': 0}
     assert func(a=10, b=5, cin=1) == {'res': 0, 'cout': 1}
+
+class TestLogicalComponents:
+  def test_mux(self):
+    # 16-bit 2-way mux
+    res = mux(16, input_bits('sel'), input_bits('a', 16), input_bits('b', 16))
+    func = _get_func(output(res=res))
+    assert func(sel=0, a=0xdead, b=0xbeef) == {'res': 0xdead}
+    assert func(sel=1, a=0xc0de, b=0xcafe) == {'res': 0xcafe}
+
+    # 8-bit 4-way mux
+    res = mux(8, input_bits('sel', 2), input_bits('a', 8), input_bits('b', 8), input_bits('c', 8), input_bits('d', 8))
+    func = _get_func(output(res=res))
+    assert func(sel=0, a=1, b=2, c=3, d=4) == {'res': 1}
+    assert func(sel=1, a=1, b=2, c=3, d=4) == {'res': 2}
+    assert func(sel=2, a=1, b=2, c=3, d=4) == {'res': 3}
+    assert func(sel=3, a=1, b=2, c=3, d=4) == {'res': 4}
+
+    # compilation error if there are not enough bits in sel
+    res = mux(8, input_bits('sel', 1), input_bits('a', 8), input_bits('b', 8), input_bits('c', 8), input_bits('d', 8))
+    with pytest.raises(IndexError):
+      func = _get_func(output(res=res))
