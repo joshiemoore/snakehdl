dbec4a36d7ab04586ecbb92b0b4d6343984f35c2
joshiemoore <joshiemoore98@gmail.com>
2025-02-22 13:08:29 -0500

    Update HACK_ALU example

--- examples/HACK_ALU.py
+++ examples/HACK_ALU.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+import sys
 from snakehdl import BOp, input_bits, output, const_bits, neg, conj, bit
 from snakehdl.components import adder, mux
-from snakehdl.compilers import VerilogCompiler
+from snakehdl.compilers import LogisimCompiler, VerilogCompiler
 
 
 def hack_alu(DATA_BITS: int) -> BOp:
@@ -47,10 +49,28 @@ def hack_alu(DATA_BITS: int) -> BOp:
   return output(out=out_neg, ng=bit(out_neg, DATA_BITS-1))
 
 if __name__ == '__main__':
-  alu = hack_alu(8)
-  print('compiling 8-bit HACK ALU from BOp tree to Verilog...', end='', flush=True)
-  cres = VerilogCompiler(alu, 'HACK_ALU').compile()
+  compiler_classes = {
+    'logisim': LogisimCompiler,
+    'verilog': VerilogCompiler,
+  }
+  alu_bits = {
+    'logisim': 4,
+    'verilog': 8,
+  }
+
+  if len(sys.argv) != 2 or sys.argv[1] not in compiler_classes:
+    print('Usage: ./HACK_ALU.py <logisim/verilog>')
+    print('e.g. ./HACK_ALU.py logisim')
+    exit(1)
+
+  alu = hack_alu(alu_bits[sys.argv[1]])
+  print(f'compiling {alu_bits[sys.argv[1]]}-bit HACK ALU from BOp tree to {sys.argv[1]}...', end='', flush=True)
+  cres = compiler_classes[sys.argv[1]](alu).compile()
   print(' done')
 
-  cres.save('HACK_ALU.v')
-  print('HACK ALU Verilog saved to HACK_ALU.v')
+  if sys.argv[1] == 'logisim':
+    cres.save('HACK_ALU.circ')
+    print('HACK ALU Logisim circuit saved to HACK_ALU.circ')
+  elif sys.argv[1] == 'verilog':
+    cres.save('HACK_ALU.v')
+    print('HACK ALU Verilog saved to HACK_ALU.v')
