c27d9e2017f09fa05c1a327a8efd0519b89dcc7f
joshiemoore <joshiemoore98@gmail.com>
2025-02-23 05:22:19 -0500

    Remove superfluous variable

--- snakehdl/components/arithmetic.py
+++ snakehdl/components/arithmetic.py
@@ -8,15 +8,14 @@ from snakehdl import (
 def adder(bits: int, a: BOp, b: BOp, cin: BOp) -> tuple[BOp, BOp]:
   """
   N-bit full adder.
-  Returns (sum, cout)
+  Returns (sum, carry)
   """
   assert bits > 0
   out = []
-  cout = cin
   for i in range(bits):
     bit_a = bit(a, i)
     bit_b = bit(b, i)
-    res = xor(xor(bit_a, bit_b), cout)
-    cout = disj(conj(bit_a, bit_b), conj(xor(bit_a, bit_b), cout))
+    res = xor(xor(bit_a, bit_b), cin)
+    cin = disj(conj(bit_a, bit_b), conj(xor(bit_a, bit_b), cin))
     out.append(res)
-  return join(*out), cout
+  return join(*out), cin
