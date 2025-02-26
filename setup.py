1d26e876f0e11306e36dd56fe4bf9ca090c3e2d3
joshiemoore <joshiemoore98@gmail.com>
2025-02-26 18:52:09 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(directory / 'README.md', encoding='utf-8') as f:
 
 setup(
   name='snakehdl',
-  version='0.1.0',
+  version='0.2.0',
   description='A simple, purely-functional HDL for Python',
   author='Josh Moore',
   license='MIT',
