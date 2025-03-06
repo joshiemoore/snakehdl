0dc5cb89880efbc29b27406dc0bab3d55ca4c1a8
joshiemoore <joshiemoore98@gmail.com>
2025-03-06 01:24:53 -0500

    Add assemble_uroutine, refactor directory structure

--- examples/snakepu/microsnake/assemble.py
+++ examples/snakepu/microsnake/assemble.py
@@ -4,6 +4,7 @@
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import List
+from uroutines.python import PY_UROUTINES
 
 
 class Register(Enum):
@@ -57,6 +58,7 @@ class JMPOp(Enum):
 
 @dataclass(frozen=True)
 class UInst:
+  op: str
   DST: Register
   SRC: Register | int
   ALU_OP: ALUOp = ALUOp.NONE
@@ -66,22 +68,50 @@ class UInst:
   DEC_SP: bool = False
   IMM: int = 0
 
-def parse_uinst(inst: str) -> UInst:
-  inst_u = inst.strip().upper()
-  sp_idx: int = inst_u.index(' ')
-  op: str = inst_u[:sp_idx]
-  args: List[str] = inst_u[sp_idx + 1:].replace(' ', '').split(',')
+def parse_uinst(inst: str, labels: dict[str, int]={}) -> UInst:
+  try:
+    sp_idx: int = inst.index(' ')
+    op: str = inst[:sp_idx]
+    args: List[str] = inst[sp_idx + 1:].replace(' ', '').split(',')
+  except ValueError:
+    op = inst
+    args = []
   args_enc: List[Register | int] = []
   for arg in args:
     if arg in Register.__members__: args_enc.append(Register[arg])
-    else: args_enc.append(int(arg))
-  print(op)
-  print(args_enc)
+    else:
+      if arg in labels: args_enc.append(labels[arg])
+      elif arg[0] == '$': args_enc.append(0) # TODO resolve builtin variables
+      else: args_enc.append(int(arg))
   # TODO create and return UInst instance with populated fields
   return UInst(
+    op,
     Register.IMM,
     Register.IMM,
   )
 
+def assemble_uroutine(routine: str) -> List[UInst]:
+  # initial pass over uinsts to resolve labels and remove comments/empty lines
+  labels = {}
+  idx = 0
+  uinsts: List[str] = []
+  for uinst in routine.split('\n'):
+    uinst_u = uinst.strip().upper()
+    if not uinst_u or uinst_u[0] == ';': continue
+    if uinst_u[-1] == ':':
+      labels[uinst_u[:-1]] = idx
+      continue
+    uinsts.append(uinst_u)
+    idx += 1
+  print(uinsts)
+  print(labels)
+  res: List[UInst] = []
+  for uinst in uinsts:
+    if uinst[-1] == ':': continue
+    res.append(parse_uinst(uinst, labels))
+  return res
+
 if __name__ == '__main__':
-  parse_uinst('eq stack, 0')
+  for uname in PY_UROUTINES:
+    print(uname)
+    assemble_uroutine(PY_UROUTINES[uname])
